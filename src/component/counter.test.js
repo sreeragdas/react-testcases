64147467f6774a950732c347dcb6ad4dc5fa1b73
sreeragdas <hsreerag25@gmail.com>
2024-08-25 18:30:35 +0530

    code updated

--- src/component/counter.test.js
+++ src/component/counter.test.js
@@ -5,7 +5,7 @@ import Counter from './counter.jsx'
 describe(Counter ,()=>{
 it ('Counter display correct inital value' ,()=>{
 const {getByTestId}=render(<Counter props={0}/>) // render the component first
- const countValue=getByTestId("count").textContent //get  element by id.
+ const countValue=Number(getByTestId("count").textContent) //get  element by id.
 
 //  we need to countValue is equal to 0 which we are sending as props 
 expect(countValue).toEqual(0)
