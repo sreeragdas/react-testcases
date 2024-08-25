9b9a657e689cf466a474441d32fcbaf4822bb81a
sreeragdas <hsreerag25@gmail.com>
2024-08-25 12:49:08 +0530

    test cases inital code

---
+++ src/component/counter.jsx
@@ -0,0 +1,23 @@
+import React ,{useState} from 'react'
+
+function Counter({props}){
+
+    const [count , setCount] =useState(props)
+    const increment =()=>{
+        setCount(prev => {
+      return prev + 1
+        });
+    }
+    return (
+
+        <div>
+            
+            <div>{count}</div>
+         
+        <button onClick={increment} > Add </button>
+        
+        </div>
+    )
+}
+
+export default Counter
---
+++ src/component/counter.test.js
@@ -0,0 +1,8 @@
+import Counter from './Counter.js'
+
+
+describe(Counter ,()=>{
+it ('Counter display correct inital value' ,()=>{
+  
+})
+})
